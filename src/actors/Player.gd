e7fd7fe12180593507f92c6a8f6da423aefa8fe4
dim11010 <johnpaulcarlos124@gmail.com>
2025-03-12 14:36:36 +0800

    modify the player

--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -1,5 +1,6 @@
 extends actor
-onready  var stomp_area: Area2D = $StompArea2D
+onready var stomp_area: Area2D = $StompArea2D
+onready var anim_sprite: AnimatedSprite = $Player
 
 export var score: = 100
 export var stomp_impulse: = 1000.0
@@ -17,9 +18,9 @@ func _physics_process(_delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
-	_velocity = calculate_move_velocity(_velocity, direction, speed,
-	 is_jump_interrupted)
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+
+	update_animation(direction)  # Call the function here
 
 #player will move left and right
 func get_direction() -> Vector2: 
@@ -27,6 +28,23 @@ func get_direction() -> Vector2:
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0 
 	)
+
+func update_animation(direction: Vector2) -> void:
+	if not is_on_floor():
+		anim_sprite.play("jump")
+	elif direction.x != 0:
+		anim_sprite.play("run")
+	elif direction.x != 0:
+		anim_sprite.play("idle")
+	else:
+		anim_sprite.play("walk")
+
+	# Flip the sprite based on direction
+	if direction.x < 0:
+		anim_sprite.flip_h = true  # Face left
+	elif direction.x > 0:
+		anim_sprite.flip_h = false # Face right
+
 
 #calculation of player to jump
 func calculate_move_velocity(
